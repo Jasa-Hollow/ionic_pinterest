71476a31babcee2ba808fcda48e4110d35288cd1
Jasa-Hollow <joel2262000@gmail.com>
2021-10-22 17:16:58 -0600

    Page - Search

--- src/app/search/search.module.ts
+++ src/app/search/search.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { SearchPageRoutingModule } from './search-routing.module';
 
 import { SearchPage } from './search.page';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SearchPageRoutingModule
+    SearchPageRoutingModule,
+    SwiperModule
   ],
   declarations: [SearchPage]
 })
--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -1,13 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { SwiperComponent } from "swiper/angular";
+
+import SwiperCore, { Pagination } from "swiper";
+
+SwiperCore.use([Pagination]);
+
+interface Slides {
+  img: string,
+  text: string
+}
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.page.html',
   styleUrls: ['./search.page.scss'],
 })
+
 export class SearchPage implements OnInit {
 
   constructor() { }
+
+  slide : Slides[] = [
+    {
+      img: 'assets/ideas/i1.png',
+      text: 'Mensaje inspiradores'
+    },
+    {
+      img: 'assets/ideas/i2.png',
+      text: 'Recetas culinarias'
+    },
+    {
+      img: 'assets/ideas/i3.png',
+      text: 'Videojuegos de Epic Games'
+    }
+  ]
 
   ngOnInit() {
   }
